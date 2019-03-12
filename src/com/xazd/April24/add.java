377e09e94067ae11ef4e266594a59d0dd5d37716
yzy <785173617@qq.com>
2019-03-12 09:52:00 +0800

    增加测试方法

--- src/com/xazd/April24/add.java
+++ src/com/xazd/April24/add.java
@@ -3,16 +3,16 @@ package com.xazd.April24;
 public class add {
 	public static void jia(int a) {
 		for (int i = 0; i <= a;) {
-			for (int j = a; j >=0; j--) {
-				System.out.println(i+"+"+j+"="+(i+j));
+			for (int j = a; j >= 0; j--) {
+				System.out.println(i + "+" + j + "=" + (i + j));
 				i++;
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-	jia(3);
-
+		jia(3);
+		jia(5);
 	}
 
 }
